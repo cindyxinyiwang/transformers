5b0d8d3efabaa1f80bc3b842a83bfdf521da54b2
xwang29 <cindyxinyiwang@gmail.com>
2020-10-12 15:10:08 -0400

    bug fix

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -149,6 +149,7 @@ if is_torch_available():
         BertForSequenceClassification,
         BertForMultipleChoice,
         BertForTokenClassification,
+        BertForMLMandTokenClassification,
         BertForQuestionAnswering,
         load_tf_weights_in_bert,
         BERT_PRETRAINED_MODEL_ARCHIVE_MAP,
@@ -353,7 +354,6 @@ if is_tf_available():
         TFDistilBertForMaskedLM,
         TFDistilBertForSequenceClassification,
         TFDistilBertForTokenClassification,
-        TFDistilBertForMLMandTokenClassification,
         TFDistilBertForQuestionAnswering,
         TF_DISTILBERT_PRETRAINED_MODEL_ARCHIVE_MAP,
     )
